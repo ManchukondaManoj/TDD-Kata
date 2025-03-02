f67575f914776cb9689db8e6596dbcff411ee6e4
Manoj <manchukondamanoj@gmail.com>
2025-03-02 09:09:16 +0530

    Setup Add function  - Basic tests for TDD approach

---
+++ stringCalculator.test.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const { test } = require("node:test");
+const { Add } = require("./stringCalculator");
+
+test("String Calculator", async (t) => {
+  await t.test("Should return 0 if input is invalid", () => {
+    assert.strictEqual(Add(""), 0);
+  });
+
+  await t.test("Should handle single input", () => {
+    assert.strictEqual(Add("2"), 2);
+  });
+
+  await t.test("Should handle two numbers", () => {
+    assert.strictEqual(Add("2, 1"), 3);
+  });
+});
