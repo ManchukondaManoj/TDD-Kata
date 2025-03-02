0c60e83adfad94c4b1fbf9b5543f976b99d0a618
Manoj <manchukondamanoj@gmail.com>
2025-03-02 09:59:49 +0530

    Add functionality to throw error if negative numbers are given

--- stringCalculator.js
+++ stringCalculator.js
@@ -9,6 +9,11 @@ function Add(stringNum) {
     stringNum = stringNum.slice(4);
   }
   const numArray = stringNum.split(delimiter).map((n) => parseInt(n, 10));
+  const negativeNumArray = numArray.filter((num) => num < 0);
+  if (negativeNumArray.length) {
+    const negativeNums = negativeNumArray.join(", ");
+    throw new Error(`negative numbers not allowed: ${negativeNums}`);
+  }
   const sum = numArray.reduce((acc, curVal) => acc + curVal, 0);
   return sum;
 }
