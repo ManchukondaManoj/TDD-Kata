6b71015adf6f96636979a3f0407ba2f2d3a81a3b
Manoj <manchukondamanoj@gmail.com>
2025-03-02 09:21:30 +0530

    Update add Functionality to return sum value

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,4 +1,12 @@
-function Add(stringNum) {}
+function Add(stringNum) {
+  if (!stringNum) {
+    return 0;
+  }
+
+  const numArray = stringNum.split(", ").map((n) => parseInt(n, 10));
+  const sum = numArray.reduce((acc, curVal) => acc + curVal, 0);
+  return sum;
+}
 
 module.exports = {
   Add,
