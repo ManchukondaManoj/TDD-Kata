a09039048ff9a14795311a9f3766b28e799c4f5d
Manoj <manchukondamanoj@gmail.com>
2025-03-02 09:26:42 +0530

    Fix function to allow multiple numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,7 +3,7 @@ function Add(stringNum) {
     return 0;
   }
 
-  const numArray = stringNum.split(", ").map((n) => parseInt(n, 10));
+  const numArray = stringNum.split(",").map((n) => parseInt(n, 10));
   const sum = numArray.reduce((acc, curVal) => acc + curVal, 0);
   return sum;
 }
